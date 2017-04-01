d94f72dca7825562f85ecb947165b386347cac16
Student <Student>
2017-04-01 08:26:18 -0700

    1st commit, just to put it up there.

---
+++ desktop/src/com/ricardojharris/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.ricardojharris.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.ricardojharris.game.Starfield;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Starfield(), config);
+	}
+}
