b744b6ca4930f6f46604f5e375180b44ea334492
Student <Student>
2017-04-07 15:02:21 -0700

    2nd commit, there's a dot on the screen

--- core/src/com/ricardojharris/game/Starfield.java
+++ core/src/com/ricardojharris/game/Starfield.java
@@ -5,29 +5,54 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+
+import java.util.Random;
 
 public class Starfield extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+
+	ShapeRenderer myRenderer;
+	Random myRandom;
+	int y;
+	int x ;
+	int screenWidth, screenHeight;
+
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		myRenderer = new ShapeRenderer();
+
+		myRandom = new Random();
+		y= myRandom.nextInt(101);
+		x= myRandom.nextInt(101);
+		screenWidth = Gdx.graphics.getWidth();
+		screenHeight = Gdx.graphics.getHeight();
+		x = myRandom.nextInt(screenWidth);
+		y = myRandom.nextInt(screenHeight);
+		starPoint();
 	}
 
 	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+	public void render (){
+
+		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+
+		myRenderer.begin(ShapeRenderer.ShapeType.Line);
+		myRenderer.rect(x,y,1,0);
+		myRenderer.end();
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		starPoint();
+	}
+
+	public void starPoint(){
+		screenWidth = Gdx.graphics.getWidth();
+		screenHeight = Gdx.graphics.getHeight();
+		x = myRandom.nextInt(screenWidth);
+		y = myRandom.nextInt(screenHeight);
 	}
 	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
+
 }
